e4e561f457f0ac5129054ca106427f987f18a5e8
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-19 18:21:14 +0200

    Initial project

---
+++ src/components/Link.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title = '';
+    export let href=''    
+</script>
+<li>
+  <a href={href}>{title}</a
+  ><br />
+  <small>23 votes by rawi 23 minutes ago</small>
+</li>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Link from "../components/Link.svelte";
+</script>
+
+<ul>
+  <Link title="Cold Hawaii Games 2023" href="https://coldhawaiigames.com/"/>
+</ul>
