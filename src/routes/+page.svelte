4a3186c1bb8ff3cafcf676ba0873e33a8b1dcd0f
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-21 12:48:43 +0200

    Uses timestamp from json response

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <ul>
   {#if data !== null && data.links.length > 0}
     {#each data.links as l}
-      <Link title={l.title} href={l.uri} created={new Date()} />
+      <Link title={l.title} href={l.uri} created={l.created} />
     {/each}
   {/if}
 </ul>
