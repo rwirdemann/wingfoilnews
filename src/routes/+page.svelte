0285e30b2573dc1417d15643de7a183c4416a950
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-21 22:07:13 +0200

    Changes to numbered list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,10 @@
   export let data = [];
 </script>
 
-<ul>
+<ol>
   {#if data !== null && data.links.length > 0}
     {#each data.links as l}
       <Link title={l.title} href={l.uri} created={l.created} />
     {/each}
   {/if}
-</ul>
+</ol>
