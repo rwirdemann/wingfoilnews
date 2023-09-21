5d4e2d2e865de4e38d48508cbfb36f2f5946666c
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-21 12:59:16 +0200

    Extracts hostname from uri

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -3,9 +3,15 @@
   export let title = "";
   export let href = "";
   export let created = new Date();
+
+  function extractDomain(uri) {
+    let url = new URL(uri);
+    return url.hostname;
+  }
 </script>
 
 <li>
-  <a {href} target="_blank">{title}</a> <small style="color: gray;">coldhawaiigames.com</small><br />
+  <a {href} target="_blank">{title}</a>
+  <small style="color: gray;">via {extractDomain(href)}</small><br />
   <small>1 vote by rawi <Time relative timestamp={created} /></small>
 </li>
