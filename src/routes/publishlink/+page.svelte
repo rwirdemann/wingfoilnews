47d4c46542e90f0d2eac899c0e4e13509f655606
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-09-24 10:58:24 +0200

    Adds post link skeleton

---
+++ src/routes/publishlink/+page.svelte
@@ -0,0 +1,22 @@
+<script context="module">
+  import { goto } from "$app/navigation";
+  let title = "";
+
+  async function doPost() {
+    goto("/success");
+  }
+</script>
+
+<h3>Publish Link</h3>
+<form>
+  <label for="title">Title</label>
+  <input
+    size="40"
+    bind:value={title}
+    id="title"
+    placeholder="Cold Hawaii Games"
+  />
+  <label for="url">URL</label>
+  <input size="40" id="url" placeholder="https://coldhawaiigames.com" />
+  <input on:click={doPost} type="submit" value="Publish" />
+</form>
---
+++ src/routes/success/+page.svelte
@@ -0,0 +1,5 @@
+<p>
+    Dein Link wurde gespeichert und wird nach einer kurzen Prüfung veröffentlicht.
+</p>
+
+<a href="/">Zurück zur Übersicht</a>
