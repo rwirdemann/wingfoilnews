68cf9e5e6e72637c6fcf6924cfb7a26fa64b96e9
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-12 19:46:36 +0200

    Shows tags

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,17 +1,24 @@
 <script>
-  import Time from "svelte-time";
-  export let title = "";
-  export let href = "";
-  export let created = new Date();
+    import Time from "svelte-time";
 
-  function extractDomain(uri) {
-    let url = new URL(uri);
-    return url.hostname;
-  }
+    export let title = "";
+    export let href = "";
+    export let tags = ""
+    export let created = new Date();
+
+    function extractDomain(uri) {
+        let url = new URL(uri);
+        return url.hostname;
+    }
 </script>
 
 <li>
-  <a {href} target="_blank">{title}</a>
-  <small style="color: gray;">via {extractDomain(href)}</small><br />
-  <small>1 vote by rawi <Time relative timestamp={created} /></small>
+    <a {href} target="_blank">{title}</a>
+    <small style="color: gray;">via {extractDomain(href)}</small><br/>
+    <small>1 vote by rawi
+        <Time relative timestamp={created}/>
+    </small>
+    {#if tags}
+        <small>({tags})</small>
+    {/if}
 </li>
