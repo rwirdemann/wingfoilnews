8615230192342d79d5c1e5ae0eadbd22c07c723d
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-16 13:44:36 +0200

    Adds sample server hook

---
+++ src/hooks.server.js
@@ -0,0 +1,14 @@
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({event, resolve}) {
+    const { headers } = event.request;
+    const cookies = headers.get("cookie") ?? ""
+    if (cookies.AuthorizationToken) {
+        const token = cookies.AuthorizationToken.split(" ")[1];
+        console.log("Token: " + token)
+    } else {
+        console.log("Request contains no AuthorizationToken")
+    }
+
+    const response = await resolve(event);
+    return response;
+}
