cfb45765aad741572c45f036f676763b01966c96
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-19 20:11:55 +0200

    Excludes blog, site and group from all news page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
-  import Link from "../components/Link.svelte";
-  export let data = [];
+    import Link from "../components/Link.svelte";
+
+    export let data = [];
+    const excluded = "site,group,blog"
 </script>
 
 <ol>
-  {#if data.links !== null && data.links.length > 0}
-    {#each data.links as l}
-      <Link title={l.title} href={l.uri} tags={l.tags} created={l.created} />
-    {/each}
-  {/if}
+    {#if data.links !== null && data.links.length > 0}
+        {#each data.links as l}
+            {#if !excluded.includes(l.tags)}
+                <Link title={l.title} href={l.uri} tags={l.tags} created={l.created}/>
+            {/if}
+        {/each}
+    {/if}
 </ol>
