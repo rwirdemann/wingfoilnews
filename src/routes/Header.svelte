eeb47f91673b68b012ce75aa464407a60c337856
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-20 16:53:48 +0200

    Shows / Hides Login link depending on jwt status

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,17 @@
+<script>
+    import {jwt} from "../store.js";
+
+    let validJwt = false
+    jwt.subscribe((value) => {
+        validJwt = value !== ""
+    })
+
+    function doLogout() {
+        localStorage.clear()
+        jwt.set("")
+    }
+</script>
+
 <header>
     <h1>Wingfoil News</h1>
     <ul class="nav">
@@ -7,7 +21,11 @@
         <li><a class="nav-link" href="/groups">Groups</a></li>
         <li><a class="nav-link" href="/sites">Sites</a></li>
         <li><a class="nav-link" href="/about">About</a></li>
-        <li><a class="nav-link" href="/login">Login</a></li>
+        {#if validJwt}
+            <li><a class="nav-link" on:click={doLogout} href="/">Logout</a></li>
+        {:else}
+            <li><a class="nav-link" href="/login">Login</a></li>
+        {/if}
     </ul>
     <hr>
 </header>
