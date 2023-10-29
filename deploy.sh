ff92c94f40bb8d37d4c5e43b3acfedef6bdc87d3
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2023-10-29 14:33:20 +0100

    Enables cross-site posts

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ scp -r build/* wingfoilnews@95.217.180.178:~/news.wingbuddies.de/build
 scp -r package*.json wingfoilnews@95.217.180.178:~/news.wingbuddies.de
 ssh wingfoilnews@95.217.180.178 "cd news.wingbuddies.de; npm ci --omit dev"
 ssh wingfoilnews@95.217.180.178 "pkill node"
-ssh wingfoilnews@95.217.180.178 "cd news.wingbuddies.de; sh -c 'nohup node build > news.wingbuddies.out 2>&1 &'"
+ssh wingfoilnews@95.217.180.178 "cd news.wingbuddies.de; sh -c 'ORIGIN=https://news.wingbuddies.de node build/index.js > news.wingbuddies.out 2>&1 &'"
